fd6a1fccd1b2033fd273700f4ea897f2ddeea674
Sebastian <hvber01@gmail.com>
2022-02-08 18:06:53 +0100

    Fixed fetching memes on page 1

--- src/components/organisms/nav/Nav.js
+++ src/components/organisms/nav/Nav.js
@@ -115,64 +115,64 @@ export default function Nav() {
                         <SearchUserModal username={searchValue} onClose={()=>handleCloseModal('search')} />
                     </Modal>
                 )}
-                <div className="wrapper">
-                    {page && page === 1 ? (
-                        // page == undefined
-                        // fix this
-                        <Link to='/strona/1' className='logo' onClick={() => {
-                            fetchPosts()
-                            window.scrollTo(0, 0)
-                        }}>Bezbekownia</Link>
-                    ) : (
-                        <Link to='/strona/1' className='logo'>Bezbekownia</Link>
-                    )}
-                    <form onSubmit={handleSearchSubmit} id="search-box" data-testid="search-box">
-                        <div className={isSearchBoxOpen ? `search-box active` : `search-box`}>
-                            <div className="icon"><Icon icon="akar-icons:search" /></div>
-                            <input type="search" autoComplete='off' placeholder='Szukaj...' value={searchValue} onChange={handleChangeSearchValue} />
-                            <button type='submit'><Icon icon="akar-icons:send" /></button>
-                        </div>
-                    </form>
-                    <ul className="right">
-                        <li className={isSearchBoxOpen ? `square search active` : `square search`} onClick={() => setSearchBoxOpen(!isSearchBoxOpen)}>
-                            <Icon icon="akar-icons:search" />
-                        </li>
-                        <li className="square more">
-                            <Icon icon="akar-icons:more-horizontal" />
-                            <DropdownMenu>
-                                <li><Link to='/pomoc'>O Bezbekowni</Link></li>
-                                <li><Link to='/pomoc'>Regulamin</Link></li>
-                                <li><Link to='/pomoc'>Polityka prywatności</Link></li>
-                            </DropdownMenu>
-                        </li>
-                        {user && user.username ? (
-                            <>
-                                <li className="square plus" title='Dodaj mema' onClick={()=>handleOpenModal('addmeme')}>
-                                    <Icon icon="akar-icons:plus" />
-                                </li>
-                                <li className="square profile">
-                                    <img src={user.image ? `${API_IP}${user.image?.url}` : UserImage} alt={user.username} />
-                                    <DropdownMenu>
-                                        <li><Link to={`/@${user.username}`}>Profil</Link></li>
-                                        <li><Link to={`/@${user.username}/edytuj`}>Ustawienia profilu</Link></li>
-                                        <hr />
-                                        <li><Link to='/moje-memy'>Moje memy</Link></li>
-                                        <li><Link to='/moje-polubienia'>Moje polubienia</Link></li>
-                                        <hr />
-                                        <li onClick={()=>logout()}><Link to='/'>Wyloguj się</Link></li>
-                                    </DropdownMenu>
-                                </li>
-                            </>
+                <div className='nav'>
+                    <div className="wrapper">
+                        {page && page === 1 ? (
+                            <Link to='/strona/1' className='logo' onClick={() => {
+                                fetchPosts()
+                                window.scrollTo(0, 0)
+                            }}>Bezbekownia</Link>
                         ) : (
-                            <li className="square buttons">
-                                <Icon icon="akar-icons:people-group" />
-                                <DropdownMenu centerItems>
-                                    <li onClick={()=>handleOpenModal('login', 'login')}><p>Zaloguj się</p></li>
-                                    <li onClick={()=>handleOpenModal('login', 'register')}><p>Zarejestruj się</p></li>
+                            <Link to='/strona/1' className='logo'>Bezbekownia</Link>
+                        )}
+                        <form onSubmit={handleSearchSubmit} id="search-box" data-testid="search-box">
+                            <div className={isSearchBoxOpen ? `search-box active` : `search-box`}>
+                                <div className="icon"><Icon icon="akar-icons:search" /></div>
+                                <input type="search" autoComplete='off' placeholder='Szukaj...' value={searchValue} onChange={handleChangeSearchValue} />
+                                <button type='submit'><Icon icon="akar-icons:send" /></button>
+                            </div>
+                        </form>
+                        <ul className="right">
+                            <li className={isSearchBoxOpen ? `square search active` : `square search`} onClick={() => setSearchBoxOpen(!isSearchBoxOpen)}>
+                                <Icon icon="akar-icons:search" />
+                            </li>
+                            <li className="square more">
+                                <Icon icon="akar-icons:more-horizontal" />
+                                <DropdownMenu>
+                                    <li><Link to='/pomoc'>O Bezbekowni</Link></li>
+                                    <li><Link to='/pomoc'>Regulamin</Link></li>
+                                    <li><Link to='/pomoc'>Polityka prywatności</Link></li>
                                 </DropdownMenu>
                             </li>
-                        )}
-                    </ul>
+                            {user && user.username ? (
+                                <>
+                                    <li className="square plus" title='Dodaj mema' onClick={()=>handleOpenModal('addmeme')}>
+                                        <Icon icon="akar-icons:plus" />
+                                    </li>
+                                    <li className="square profile">
+                                        <img src={user.image ? `${API_IP}${user.image?.url}` : UserImage} alt={user.username} />
+                                        <DropdownMenu>
+                                            <li><Link to={`/@${user.username}`}>Profil</Link></li>
+                                            <li><Link to={`/@${user.username}/edytuj`}>Ustawienia profilu</Link></li>
+                                            <hr />
+                                            <li><Link to='/moje-memy'>Moje memy</Link></li>
+                                            <li><Link to='/moje-polubienia'>Moje polubienia</Link></li>
+                                            <hr />
+                                            <li onClick={()=>logout()}><Link to='/'>Wyloguj się</Link></li>
+                                        </DropdownMenu>
+                                    </li>
+                                </>
+                            ) : (
+                                <li className="square buttons">
+                                    <Icon icon="akar-icons:people-group" />
+                                    <DropdownMenu centerItems>
+                                        <li onClick={()=>handleOpenModal('login', 'login')}><p>Zaloguj się</p></li>
+                                        <li onClick={()=>handleOpenModal('login', 'register')}><p>Zarejestruj się</p></li>
+                                    </DropdownMenu>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
                 </div>
             </StyledNav>
         </>
