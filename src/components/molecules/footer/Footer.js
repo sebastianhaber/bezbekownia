2b02ad74ba9ec3c70ab84891f494b97833727881
Sebastian <hvber01@gmail.com>
2022-02-11 14:32:35 +0100

    Footer

---
+++ src/components/molecules/footer/Footer.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components"
+
+export default function Footer() {
+  return (
+    <StyledFooter>
+        <div className="logo">Bezbekownia</div>
+        <ul>
+            <li><Link to='/legal/regulamin'>Regulamin</Link></li>
+            <li><Link to='/legal/polityka-prywatnosci'>Polityka prywatności</Link></li>
+            <li><Link to='/legal/rodo'>Ochrona danych osobowych</Link></li>
+        </ul>
+        <div className="copyright">2022 &copy; Bezbekownia.pl</div>
+    </StyledFooter>
+  )
+}
+const StyledFooter = styled.footer`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    margin-top: 2rem;
+    padding: 2rem 1rem;
+    background-color: ${({theme}) => theme.colors.background.light};
+    font-size: 14px;
+    text-align: center;
+    .logo{
+        font-weight: bold;
+        font-size: 1.5rem;
+    }
+    ul{
+        display: flex;
+        gap: 1rem;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    .copyright{
+        color: ${({theme}) => theme.colors.gray};
+    }
+`;
